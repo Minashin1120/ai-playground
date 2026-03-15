08c96d65b590cb70065668a85b7a51d0aa7e7afe
minashin1120 <179431119+Minashin1120@users.noreply.github.com>
2026-03-15 18:03:33 +0900

    Update V4.8.355: fix JS syntax error

--- templates/chat_app.js
+++ templates/chat_app.js
@@ -9605,7 +9605,6 @@
             
             // Trigger initial log to confirm system is active
             setTimeout(() => {
-                console.log("Extended debug logging system active. Version: v4.8.354");
+                console.log("Extended debug logging system active. Version: v4.8.355");
             }, 3000);
-        })();
         })();

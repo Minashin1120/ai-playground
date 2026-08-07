947e4524f4f18156fcff128fb11e01154d9e7287
minashin1120 <179431119+Minashin1120@users.noreply.github.com>
2026-08-08 03:52:44 +0900

    V4.8.764: gunicorn graceful-timeoutを30秒→10秒に短縮し再起動時間を改善

--- app.py
+++ app.py
@@ -733,8 +733,8 @@ class _StaticAssetSessionInterface(SecureCookieSessionInterface):
         return super().save_session(flask_app, session_obj, response)
 
 app.session_interface = _StaticAssetSessionInterface()
-app.config['APP_VERSION'] = os.getenv('APP_VERSION', '2026-08-08-002')
-app.config['SYSTEM_VERSION'] = 'V4.8.763'
+app.config['APP_VERSION'] = os.getenv('APP_VERSION', '2026-08-08-003')
+app.config['SYSTEM_VERSION'] = 'V4.8.764'
 app.config['SESSION_COOKIE_SECURE'] = True
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'

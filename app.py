4841992ddb0001847622d982dff28981f7ddc69e
minashin1120 <179431119+Minashin1120@users.noreply.github.com>
2026-09-15 00:02:28 +0900

    Fix batch completion banner overlapping the header

--- app.py
+++ app.py
@@ -753,8 +753,8 @@ class _StaticAssetSessionInterface(SecureCookieSessionInterface):
         return super().save_session(flask_app, session_obj, response)
 
 app.session_interface = _StaticAssetSessionInterface()
-app.config['APP_VERSION'] = os.getenv('APP_VERSION', '2026-09-14-013')
-app.config['SYSTEM_VERSION'] = 'V4.8.973'
+app.config['APP_VERSION'] = os.getenv('APP_VERSION', '2026-09-14-014')
+app.config['SYSTEM_VERSION'] = 'V4.8.974'
 app.config['SESSION_COOKIE_SECURE'] = True
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'

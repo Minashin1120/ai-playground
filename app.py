943bc3752c658212c01188f871bdd816cb76c1cc
minashin1120 <179431119+Minashin1120@users.noreply.github.com>
2026-09-18 21:47:42 +0900

    Fix dark-mode contrast in chat UI

--- app.py
+++ app.py
@@ -753,8 +753,8 @@ class _StaticAssetSessionInterface(SecureCookieSessionInterface):
         return super().save_session(flask_app, session_obj, response)
 
 app.session_interface = _StaticAssetSessionInterface()
-app.config['APP_VERSION'] = os.getenv('APP_VERSION', '2026-09-18-001')
-app.config['SYSTEM_VERSION'] = 'V4.8.1001'
+app.config['APP_VERSION'] = os.getenv('APP_VERSION', '2026-09-18-002')
+app.config['SYSTEM_VERSION'] = 'V4.8.1002'
 app.config['SESSION_COOKIE_SECURE'] = True
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
